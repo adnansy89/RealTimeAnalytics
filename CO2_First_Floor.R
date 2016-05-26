cef03e1448dcf4e0ad2c3a9be4f3e9d414d2289a
adnansy89 <adnan89@uw.edu>
2016-05-26 00:36:30 -0700

    Create CO2_First_Floor.R

---
+++ CO2_First_Floor.R
@@ -0,0 +1,12 @@
+#Reading the Data for the First Floor
+FullData <- read.csv("Dataset_CO2_First_Floor.csv", header = TRUE)
+
+# TODO --> 90% and 10% of the rows in the csv file should be passed automatically
+
+#Data Split into 90% and 10% for Training and Testing
+df_train <- read.csv("Dataset_CO2_First_Floor.csv", nrows = 5471)
+df_test <- read.csv("Dataset_CO2_First_Floor.csv", skip = 5471)
+
+#Splitting the training data further into train and test by 80% and 20%
+df_train_train <- df_train[1:4377,]
+df_train_test <- df_train[4378:5471,]
